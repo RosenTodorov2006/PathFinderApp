09e9990a71901029de18ec1b85f3e05a2fbc7b83
Rosen Todorov <122826159+RosenTodorov2006@users.noreply.github.com>
2024-10-23 00:00:42 +0300

    delete unnecessary method

--- src/main/java/bg/soft_uni/pathfinderapp/Repositories/UserRepository.java
+++ src/main/java/bg/soft_uni/pathfinderapp/Repositories/UserRepository.java
@@ -10,5 +10,4 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String name);
     Optional<User> findByEmail(String name);
-    Optional<User> findByUsernameAndPassword(String username, String password);
 }
